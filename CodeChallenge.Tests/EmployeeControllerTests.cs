cb6e4199199541631a4ebb4e228b0492d814952c
Mark Makris <mmakris98@gmail.com>
2022-07-22 09:17:08 -0400

    basic tests implimented

--- CodeChallenge.Tests/EmployeeControllerTests.cs
+++ CodeChallenge.Tests/EmployeeControllerTests.cs
@@ -132,5 +132,85 @@ namespace CodeCodeChallenge.Tests.Integration
             // Assert
             Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
+
+        [TestMethod]
+        public void GetReportStructureById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+            var expectedNumberOfReports = 4;
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/employee/reportStructure/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(expectedFirstName, reportingStructure.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, reportingStructure.Employee.LastName);
+            Assert.AreEqual(expectedNumberOfReports, reportingStructure.NumberOfReports);
+        }
+
+        [TestMethod]
+        public void CreateCompensation_Returns_Created()
+        {
+            // Arrange
+            var compensation = new Compensation()
+            {
+                CompensationId = "",
+                Employee = new Employee()
+                {
+                    EmployeeId = "21a786ae-ed73-4567-34fe-c423658245af",
+                    Department = "Money",
+                    FirstName = "Salt",
+                    LastName = "Jones",
+                    Position = "Boss"
+                },
+                Salary = 35000,
+                EffectiveDate = System.DateTime.Parse("1998-05-22")
+            };
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/employee/compensation",
+               new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newCompensation = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(newCompensation.CompensationId);
+            Assert.AreEqual(compensation.Employee.FirstName, newCompensation.Employee.FirstName);
+            Assert.AreEqual(compensation.Employee.LastName, newCompensation.Employee.LastName);
+            Assert.AreEqual(compensation.Employee.Department, newCompensation.Employee.Department);
+            Assert.AreEqual(compensation.Employee.Position, newCompensation.Employee.Position);
+            Assert.AreEqual(compensation.EffectiveDate, newCompensation.EffectiveDate);
+            Assert.AreEqual(compensation.Salary, newCompensation.Salary);
+        }
+
+        [TestMethod]
+        public void GetCompensationById_Returns_Ok()
+        {
+            // Arrange
+            var expectedCompensationId = "21a786ae-ed73-4567-34fe-c423658245af";
+            var expectedSalary = 35000;
+            var expectedEffectiveDate = System.DateTime.Parse("1998-05-22");
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/employee/compensation/{expectedCompensationId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var compensation = response.DeserializeContent<Compensation>();
+            Assert.AreEqual(expectedCompensationId, compensation.CompensationId);
+            Assert.AreEqual(expectedSalary, compensation.Salary);
+            Assert.AreEqual(expectedEffectiveDate, compensation.EffectiveDate);
+        }
     }
 }
